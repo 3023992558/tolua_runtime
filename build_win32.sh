84d63b5c63b38f03a7c78e5ad29860c81a5fe7d2
2801659445 <2801659445@qq.com>
2017-06-20 12:48:27 +0800

     window版本编译

--- build_win32.sh
+++ build_win32.sh
@@ -15,31 +15,16 @@ gcc -m32 -O3 -std=gnu99 -shared \
 	int64.c \
 	uint64.c \
 	tolua.c \
-	pb.c \
-	lpeg.c \
-	struct.c \
-	cjson/strbuf.c \
-	cjson/lua_cjson.c \
-	cjson/fpconv.c \
-	luasocket/auxiliar.c \
-	luasocket/buffer.c \
-	luasocket/except.c \
-	luasocket/inet.c \
-	luasocket/io.c \
-	luasocket/luasocket.c \
-	luasocket/mime.c \
-	luasocket/options.c \
-	luasocket/select.c \
-	luasocket/tcp.c \
-	luasocket/timeout.c \
-	luasocket/udp.c \
-	luasocket/wsocket.c \
+	lua_extensions/lpeg.c \
+	lua_extensions/struct.c \
+	lua_extensions/cjson/strbuf.c \
+	lua_extensions/cjson/lua_cjson_jit.c \
+	lua_extensions/cjson/fpconv.c \
 	lua_extensions/lfs/lfs.c \
-	lua_extensions/lua_extensions.c \
+	lua_extensions/lua_extensions_jit.c \
 	-o Plugins/x86/tolua.dll \
 	-I./ \
 	-Iluajit-2.1/src \
-	-Icjson \
-	-Iluasocket \
+	-Ilua_extensions \
 	-lws2_32 \
 	-Wl,--whole-archive window/x86/libluajit.a -Wl,--no-whole-archive -static-libgcc -static-libstdc++
--- build_win64.sh
+++ build_win64.sh
@@ -15,30 +15,16 @@ gcc -m64 -O2 -std=gnu99 -shared \
  tolua.c \
  int64.c \
  uint64.c \
- pb.c \
- lpeg.c \
- struct.c \
- cjson/strbuf.c \
- cjson/lua_cjson.c \
- cjson/fpconv.c \
- luasocket/auxiliar.c \
- luasocket/buffer.c \
- luasocket/except.c \
- luasocket/inet.c \
- luasocket/io.c \
- luasocket/luasocket.c \
- luasocket/mime.c \
- luasocket/options.c \
- luasocket/select.c \
- luasocket/tcp.c \
- luasocket/timeout.c \
- luasocket/udp.c \
- luasocket/wsocket.c \
+ lua_extensions/lpeg.c \
+ lua_extensions/struct.c \
+ lua_extensions/cjson/strbuf.c \
+ lua_extensions/cjson/lua_cjson_jit.c \
+ lua_extensions/cjson/fpconv.c \
  lua_extensions/lfs/lfs.c \
- lua_extensions/lua_extensions.c \
+ lua_extensions/lua_extensions_jit.c \
  -o Plugins/x86_64/tolua.dll \
  -I./ \
  -Iluajit-2.1/src \
- -Iluasocket \
+ -Ilua_extensions \
  -lws2_32 \
  -Wl,--whole-archive window/x86_64/libluajit.a -Wl,--no-whole-archive -static-libgcc -static-libstdc++
