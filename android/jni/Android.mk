04c059c4636e3b13b36cd422e2660c0419c65a4a
unknown <wangys@game-reign.com>
2018-07-19 21:06:34 +0800

    解决冲突

--- android/jni/Android.mk
+++ android/jni/Android.mk
@@ -25,6 +25,18 @@ LOCAL_SRC_FILES :=	../../tolua.c \
 					../../lua_extensions/lpack/lpack.c \
  					../../lua_extensions/lua_zlib/lua_zlib.c \
 					../../lua_extensions/lfs/lfs.c \
+					../../lua_extensions/yaml/api.c \
+					../../lua_extensions/yaml/dumper.c \
+					../../lua_extensions/yaml/emitter.c \
+					../../lua_extensions/yaml/loader.c \
+					../../lua_extensions/yaml/parser.c \
+					../../lua_extensions/yaml/reader.c \
+					../../lua_extensions/yaml/scanner.c \
+					../../lua_extensions/yaml/writer.c \
+					../../lua_extensions/lyaml/emitter.c \
+					../../lua_extensions/lyaml/parser.c \
+					../../lua_extensions/lyaml/scanner.c \
+					../../lua_extensions/lyaml/yaml.c \
 					../../lua_extensions/lua_extensions_jit.c \
  					
 LOCAL_WHOLE_STATIC_LIBRARIES += libluajit
--- build_win64.sh
+++ build_win64.sh
@@ -22,6 +22,18 @@ gcc -m64 -O2 -std=gnu99 -shared \
  lua_extensions/cjson/fpconv.c \
  lua_extensions/lfs/lfs.c \
  lua_extensions/lpack/lpack.c \
+ lua_extensions/yaml/api.c \
+ lua_extensions/yaml/dumper.c \
+ lua_extensions/yaml/emitter.c \
+ lua_extensions/yaml/loader.c \
+ lua_extensions/yaml/parser.c \
+ lua_extensions/yaml/reader.c \
+ lua_extensions/yaml/scanner.c \
+ lua_extensions/yaml/writer.c \
+ lua_extensions/lyaml/emitter.c \
+ lua_extensions/lyaml/parser.c \
+ lua_extensions/lyaml/scanner.c \
+ lua_extensions/lyaml/yaml.c \
  lua_extensions/lua_extensions_jit.c \
  -lz -lm \
  -o Plugins/x86_64/tolua.dll \
